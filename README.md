8276c7735a338dd8261ef524c71d781bb22f2188
Mohamed Elshafeay <mohamedelshafeay@gmail.com>
2020-04-19 19:28:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# This is a crowd funding web application  
+
+     Still under development...  
+
+<br>
+
+## Prerequisites 
+* python (>= 3.4 required)  
+
+
+<br>
+
+## Installation 
+**To run the application after downloading you need to do a few steps:**  
+  
+* ```pip install virtualenv```  
+* ```cd crowd-funding```  
+* ```virtualenv env```  
+* ```source env/bin/activate```  
+* ```pip install -r requirements.txt```  
+* ```cp config.example.ini config.ini```  
+  
+* Start replacing the environment variables in the config.ini  file with yours.  
+  **PS:** You can generate secret key to the project from [here](https://djecrety.ir/).  
+  
+* ```python manage.py migrate```  
+* ```python manage.py runserver```  
+  
+#### And by now you should have the application running.
---
+++ crowdfunding/settings.py
@@ -0,0 +1,103 @@
+import os
+from configparser import RawConfigParser
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+config = RawConfigParser()
+config.read(os.path.join(BASE_DIR, 'config.ini'))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+DEBUG = config.getboolean("global", "DEBUG")
+SECRET_KEY = config.get('secrets', 'SECRET_KEY')
+ALLOWED_HOSTS = []
+
+
+INSTALLED_APPS = [
+    'projects',
+    'users',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'crowdfunding.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'crowdfunding.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE':   config.get('database', 'DATABASE_ENGINE'),
+        'NAME':     config.get('database', 'DATABASE_NAME'),
+        'USER':     config.get('database', 'DATABASE_USER'),
+        'PASSWORD': config.get('database', 'DATABASE_PASSWORD'),
+        'HOST':     config.get('database', 'DATABASE_HOST'),
+        'PORT':     config.getint('database', 'DATABASE_PORT'),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
+MEDIA_URL = '/uploads/'
