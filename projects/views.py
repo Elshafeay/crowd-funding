cf1ef896d176beff00f3129ebbe94c9eaa77baea
Islam <iabdelhamid3@gmail.com>
2020-04-23 16:57:16 +0200

    add tempalte to projects

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Project
+from django.shortcuts import render, redirect
+from .models import Project, Donation
+from .forms import DonateForm
 from django.http import HttpResponse
 
 
@@ -8,5 +9,21 @@ from django.http import HttpResponse
 
 def show(request, project_id):
     project = Project.objects.get(id=project_id)
-    context = {"project": project}
+    donate_form = DonateForm({'user_id': 1})
+    context = {'project': project, 'donate_form': donate_form}
     return render(request, 'projects/show.html', context)
+
+
+def donate(request, project_id):
+    if request.method == 'POST':
+        donation_form = DonateForm(request.POST)
+        project = Project.objects.get(id=project_id)
+        context = {'project': project, 'donate_form': donation_form}
+        if donation_form.is_valid():
+            donation = Donation(donation=donation_form.cleaned_data['donation'],
+                                user_id=donation_form.cleaned_data['user_id'], project_id=project_id)
+            donation.save()
+            # project.donations.add(donation)
+            return render(request, 'projects/show.html', context)
+        else:
+            return render(request, 'projects/show.html', context)
