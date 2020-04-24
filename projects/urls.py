c8ab35977237f7e07ec554d8f75cf845470a3139
omarMohamedAbdo <omarknighto@gmail.com>
2020-04-24 20:32:08 +0200

    Create Project Update

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import show, donate, projects_list,show_all,create_project
+from .views import show, donate, projects_list,show_all,show_create_project,create
 
 urlpatterns = [
     path('<int:project_id>', show, name='show_project'),
     path('<int:project_id>/donate', donate, name='donate_project'),
     path('', show_all, name='show_project'),
-    path('create', create_project, name='create_project'),
+    path('create', show_create_project, name='show_create_project'),
+    path('create/submit', create, name='create_project'),
     path('self', projects_list)
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Project, Donation
-from .forms import DonateForm
+from .forms import DonateForm,CreateForm
+from django.contrib import messages
 
 
 def show(request, project_id):
@@ -30,8 +31,28 @@ def donate(request, project_id):
 def show_all(request):
     return render(request, 'projects/all_projects.html')
 
-def create_project(request):
-    return render(request, 'projects/create_project.html')
+def show_create_project(request):
+    context = { 'create_form': CreateForm}
+    return render(request, 'projects/create_project.html',context)
+
+def create(request):
+    if request.method == 'POST':
+        create_form = CreateForm(request.POST)
+        context = {'create_form': create_form}
+        if create_form.is_valid():
+            project = Project(
+                title=create_form.cleaned_data['title'],
+                details=create_form.cleaned_data['details'],
+                target=create_form.cleaned_data['target'],
+                start_date=create_form.cleaned_data['start_date'],
+                end_date=create_form.cleaned_data['end_date'],
+                owner_id=request.user.id
+            )
+            project.save()
+            messages.success(request, 'Form submission successful')
+            return render(request, 'projects/create_project.html', context)
+        else:
+            return render(request, 'projects/create_project.html', context)
 
 def projects_list(request):
     projects = Project.objects.all()
