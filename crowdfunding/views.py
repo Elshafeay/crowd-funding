31c1cd4fcc8228312fcdc184e73234d6acd1bd02
Reham Hussein <rehamhussein_fcis@yahoo.com>
2020-04-24 09:43:52 +0200

    get all projects related to first category

--- crowdfunding/views.py
+++ crowdfunding/views.py
@@ -8,11 +8,24 @@ from projects.models import Category
 # - Search bar that enables users to search projects by title or tag
 
 def welcome(request):
+	gcategories = getCategoriesAndProjects()
+	firstCategory =gcategories['firstCategory']
+	categories =gcategories['categories']
+	if(firstCategory):
+		return render(request, "projects/home_page.html", {"firstCategory": firstCategory, "categories": categories, "firstCategoryProjects":gcategories['firstCategoryProjects']})
+	else:
+		return render(request, "projects/home_page.html", {})
+
+
+def getCategoriesAndProjects():
 	categories = Category.objects.all()
 	if len(categories) > 1:
 		firstCategory = categories[0]
+		firstCategoryProjects = categories[0].project_set.all()
 		categories = categories.exclude(id=firstCategory.id)
-		return render(request, "projects/home_page.html", {"firstCategory": firstCategory, "categories": categories})
+		return {"firstCategory": firstCategory, "categories": categories, "firstCategoryProjects":firstCategoryProjects}
 	else:
-		return render(request, "projects/home_page.html", {})
-	# print(categories)
+		return {}
+# print(categories)
+
+
