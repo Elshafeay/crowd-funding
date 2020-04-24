248ec314298411f80a116d4bcdedef2509e5d302
omarMohamedAbdo <omarknighto@gmail.com>
2020-04-24 22:59:23 +0200

    More On Projecte Create

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Project, Donation
+from .models import Project, Donation,Category
 from .forms import DonateForm,CreateForm
 from django.contrib import messages
+from django.shortcuts import redirect
 
 
 def show(request, project_id):
@@ -31,26 +32,30 @@ def donate(request, project_id):
 def show_all(request):
     return render(request, 'projects/all_projects.html')
 
+categories = Category.objects.all()
+
 def show_create_project(request):
-    context = { 'create_form': CreateForm}
+    context = { 'create_form': CreateForm , 'categories' : categories}
     return render(request, 'projects/create_project.html',context)
 
 def create(request):
     if request.method == 'POST':
-        create_form = CreateForm(request.POST)
-        context = {'create_form': create_form}
+        create_form = CreateForm(request.POST,request.FILES)
+        context = {'create_form': create_form,'categories' : categories}
         if create_form.is_valid():
             project = Project(
                 title=create_form.cleaned_data['title'],
                 details=create_form.cleaned_data['details'],
                 target=create_form.cleaned_data['target'],
+                cover = request.FILES['cover'],
+                category_id = request.POST['categ'],
                 start_date=create_form.cleaned_data['start_date'],
                 end_date=create_form.cleaned_data['end_date'],
                 owner_id=request.user.id
             )
             project.save()
-            messages.success(request, 'Form submission successful')
-            return render(request, 'projects/create_project.html', context)
+            messages.success(request, 'Project Created Successfully')
+            return redirect('/projects/create')
         else:
             return render(request, 'projects/create_project.html', context)
 
