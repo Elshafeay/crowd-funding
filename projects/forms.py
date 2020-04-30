2d622a5f471576be38edb660d0204aa7e3f6f071
Mohamed Elshafeay <mohamedelshafeay@gmail.com>
2020-04-30 09:51:51 +0200

    Adding some styles

--- projects/forms.py
+++ projects/forms.py
@@ -11,14 +11,29 @@ class DonateForm(forms.Form):
 
 
 class CreateForm(forms.Form):
-    title = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title '})   )
-    details = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Details ','rows':'4'}))
-    cover = forms.ImageField()
-    tags = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Tags Seperated by ,'})   )
-    # category_id = forms.CharField(widget=forms.Select(choices=categories))
-    start_date = forms.DateField( widget=forms.SelectDateWidget)
-    end_date = forms.DateField( widget=forms.SelectDateWidget)
-    images = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    title = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Title '}
+    ))
+    details = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'form-control', 'placeholder': 'Details ', 'rows': '4'}
+    ))
+    cover = forms.ImageField(
+        widget=forms.ClearableFileInput(attrs={'class': 'form-control'}))
+    start_date = forms.DateField(widget=forms.SelectDateWidget(
+        attrs={'class': 'form-control'})
+    )
+    end_date = forms.DateField(widget=forms.SelectDateWidget(
+        attrs={'class': 'form-control'})
+    )
+    images = forms.ImageField(widget=forms.ClearableFileInput(
+        attrs={'multiple': True, 'class': 'form-control'}
+    ))
     target = forms.CharField(widget=forms.NumberInput(
         attrs={'class': 'form-control', 'placeholder': 'Target '}))
 
+    tags = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Tags Seperated by ,'
+        }
+    ))
